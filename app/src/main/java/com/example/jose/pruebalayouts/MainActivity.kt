4736fc04dc419143c4b92db892f360c884802dde
josedominguezmartinan <josetic12@gmail.com>
2019-01-11 11:27:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jose/pruebalayouts/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.jose.pruebalayouts
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var boton=findViewById<Button>(R.id.button)
+        var horizontal = findViewById<TextView>(R.id.textView2) // tenemos un elemento boton que solo aparece en la posicion horizontal , sera lo que usaremos para saber la posicion en la que esta y mostrar un toast
+        var is_horizontal=false;
+
+        val intent=Intent(this,Main2Activity::class.java).apply {  }
+
+        boton.setOnClickListener{
+            if(horizontal!=null) // si el boton existe marcamos la variable a true y mostramos el toast
+                is_horizontal=true
+            if(is_horizontal)
+                Toast.makeText(this,"HORIZONTAL",Toast.LENGTH_LONG).show()
+            else
+            startActivity(intent)
+        }
+    }
+}
